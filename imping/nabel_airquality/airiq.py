ec4e67fbd4c5d4516ab04dc81516ec9d8d0d9338
Mehltretter Stephanie <mehlst@akros.ch>
2025-06-19 11:35:43 +0200

    Addition of several curl commands to access the airiq api.

---
+++ imping/nabel_airquality/airiq.py
@@ -0,0 +1,8 @@
+curl 'https://api.airvisual.com/v2/current?city=Amsterdam&state=Noord‑Holland&country=Netherlands&key=7ee78f42-9bc7-4fc6-9656-8e086684a451'
+curl --location -g 'http://api.airvisual.com/v2/countries?key=7ee78f42-9bc7-4fc6-9656-8e086684a451'
+
+curl --location -g 'http://api.airvisual.com/v2/city?city=Los%20Angeles&state=California&country=USA&key=7ee78f42-9bc7-4fc6-9656-8e086684a451'
+
+curl "https://api.airvisual.com/v2/current?city=Zurich&state=Zurich&country=Switzerland&key=7ee78f42-9bc7-4fc6-9656-8e086684a451"
+
+curl "https://api.waqi.info/feed/zurich/?token=YOUR_TOKEN"
