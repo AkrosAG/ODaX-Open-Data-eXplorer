ad0fce869c7f245cbe6a35bf1f9c443e090098e0
Mehltretter Stephanie <mehlst@akros.ch>
2025-06-30 14:55:29 +0200

    Rename of libary file.

---
+++ analysis/airquality_healthinsurancefees.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import os
+from imping.healthinsurance.lib_healthinsurance import GetFeesByParameters, GetKVNameFromBAGNumber, GetAlterunterGruppenProVersicherer, GetMunicities, GetRegion, GetKantonRegionFromGemeinde
+
+
+
