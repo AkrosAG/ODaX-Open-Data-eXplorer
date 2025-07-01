10c59f6b3f97c9245ba629a7454926c49d7134a5
Stephanie Mehltretter <stephanie.mehltretter@akros.ch>
2025-07-01 13:58:54 +0200

    Separation of openweatherdata handling into two files.

--- imping/nabel_airquality/lib_openweathermap.py
+++ imping/nabel_airquality/lib_openweathermap.py
@@ -1,11 +1,5 @@
-import os
-
 import requests
-import json
-from dotenv import load_dotenv
-load_dotenv()
 
-API_KEY = os.getenv("APIKeyOpenWeatherMap")
 def get_air_quality(latitude, longitude, api_key):
   """
   Retrieves air quality data from OpenWeatherMap API.
@@ -18,8 +12,6 @@ def get_air_quality(latitude, longitude, api_key):
   Returns:
     A JSON object containing air quality data, or None if an error occurs.
   """
-  url = f"http: // api.openweathermap.org / data / 2.5 / air_pollution?lat = {latitude} & lon = {longitude} & appid = {api_key}"
-  #url = f"https://api.openweathermap.org/data/2.5/air_pollution?lat={latitude}&lon={longitude}&appid={api_key}"
   url = f"http://api.openweathermap.org/data/2.5/air_pollution?lat=50&lon=50&appid={api_key}"
   try:
     response = requests.get(url)
@@ -29,18 +21,3 @@ def get_air_quality(latitude, longitude, api_key):
   except requests.exceptions.RequestException as e:
     print(f"Error fetching data: {e}")
     return None
-
-
-"""
-# Replace it with your actual latitude, longitude, and API key
-latitude = 47.3769
-longitude = 8.5417
-
-
-api_key = API_KEY
-
-air_quality_data = get_air_quality(latitude, longitude, api_key)
-
-if air_quality_data:
-  print(json.dumps(air_quality_data, indent=2))
-"""
